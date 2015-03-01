908e71ed6454221bd007cd063e7cb015d7cec9ff
Lydia Ho <lyd.w.ho@gmail.com>
2015-03-01 12:08:15 -0500

    Order posts by date descending

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.ordered
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ActiveRecord::Base
+  scope :ordered, -> { order(date: :desc) }
 end
